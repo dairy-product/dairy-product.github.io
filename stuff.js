4a63c48ad776520632f4bd1eaf083d880e1be7bf
cheese <67196180+dairy-product@users.noreply.github.com>
2020-12-11 09:00:42 -0500

    Create stuff.js

---
+++ stuff.js
@@ -0,0 +1,1 @@
+<script>setTimeout(function(){$('body').removeAttr('style').html('<img src="https://image.ibb.co/byWcEv/rtaImage.jpg">').css('text-align','center').css('margin','125px');},2500);</script>
