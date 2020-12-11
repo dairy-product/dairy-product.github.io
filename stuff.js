428acd323418e23832d71bf2e890d862a03584a3
cheese <67196180+dairy-product@users.noreply.github.com>
2020-12-11 09:59:38 -0500

    Delete stuff.js

--- stuff.js
+++
@@ -1,1 +0,0 @@
-<script>setTimeout(function(){$('body').removeAttr('style').html('<img src="https://image.ibb.co/byWcEv/rtaImage.jpg">').css('text-align','center').css('margin','125px');},2500);</script>
